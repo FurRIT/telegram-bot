a800e9627eab5f2dd2bacad99c66aff7d4e334b0
scooterwolfie <pertainspup@protonmail.com>
2023-11-06 11:39:28 -0500

    Panning now works

--- src/main/FURRIT-BOT/main.py
+++ src/main/FURRIT-BOT/main.py
@@ -15,10 +15,26 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def pan(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    replied_message = update.message.reply_to_message
+
+    if replied_message:
+        original_message_id = replied_message.message_id
+        await update.message.reply_text(text="Panned", reply_to_message_id=original_message_id)
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="You can't pan yourself!"
+        )
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token('6368893309:AAFH_9TxDdRJfT7rReTWVkAjirFFdCpNSEM').build()
 
     start_handler = CommandHandler('start', start)
+    pan_handler = CommandHandler('pan', pan)
     application.add_handler(start_handler)
+    application.add_handler(pan_handler)
+
 
     application.run_polling()

70fb3f49aeadef249cc2d60aa05922bb697325e4
TorinSol <ethan.ricker567@gmail.com>
2023-11-07 16:28:10 -0500

    trying to get git accounts to work properly

--- src/main/FURRIT-BOT/main.py
+++ src/main/FURRIT-BOT/main.py
@@ -44,11 +44,6 @@ async def Rfine(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 #why wwont it work????
-async def fines(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="AWOOOOOOOOOO\nfining a user"
-    )
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('6368893309:AAFH_9TxDdRJfT7rReTWVkAjirFFdCpNSEM').build()

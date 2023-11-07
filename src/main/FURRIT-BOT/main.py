2b8a20a130335a32c44e8e2642171ae362a83091
TorinSol <ethan.ricker567@gmail.com>
2023-11-07 18:16:47 -0500

    Changed the remove fine function to allow for a target to be designated, this doesnt yet work however

--- src/main/FURRIT-BOT/main.py
+++ src/main/FURRIT-BOT/main.py
@@ -1,7 +1,7 @@
 import logging
 import random
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram import Update, ChatMemberUpdated
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, ChatMemberHandler
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -35,26 +35,59 @@ async def pan(update: Update, context: ContextTypes.DEFAULT_TYPE):
             text="You need to reply to a message to pan."
         )
 
+#removes a single fine from a user
+# should allow the caller to specify the amt removed, also need to
+#   change so that you call it using the @ of a user.
+#chat_member_update: ChatMemberUpdated,
+async def Rfine( update: Update, context: ContextTypes.DEFAULT_TYPE):
+    replied_message = update.message.reply_to_message
+    user_says = "".join(context.args)
+    #target = chat_member_update.difference().get("status")
+    if replied_message:
+        original_message_id = replied_message.message_id
+        await update.message.reply_text(
+            text="forgiving a $350 fine from "+replied_message.from_user.username,
+            reply_to_message_id=original_message_id)
+    elif user_says != "":
+        await context.bot.send_message(
+
+            chat_id=update.effective_chat.id,
+            text="Forgiving a $350 fine from "+user_says +
+                 "\n You havent gotten this working yet\nIDK how to get the bot to check if a user is real"
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="No user selected"
+        )
 
 
-async def Rfine(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(
-        chat_id=update.effective_chat.id,
-        text="Fining a user"
-    )
+#allows the caller to fine a user for a message
 async def fines(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    replied_message = update.message.reply_to_message
+    if replied_message:
+        original_message_id = replied_message.message_id
+        user = replied_message.from_user.username
+        await update.message.reply_text(text="Fining "+user+" $350", reply_to_message_id=original_message_id)
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text="No user selected to fine"
+        )
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Fining a user"
+        text="You havent finished this yet"
     )
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token('6368893309:AAFH_9TxDdRJfT7rReTWVkAjirFFdCpNSEM').build()
 
+    #application.add_handler(ChatMemberHandler(track_chats, ChatMemberHandler.MY_CHAT_MEMBER))
+
     start_handler = CommandHandler('start', start)
     pan_handler = CommandHandler('pan', pan)
     fine_handler = CommandHandler('fine', fines)
-    remove_fine_handler = CommandHandler('un fine', Rfine)
+    remove_fine_handler = CommandHandler('unfine', Rfine)
     application.add_handler(start_handler)
     application.add_handler(pan_handler)
     application.add_handler(fine_handler)

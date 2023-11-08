646b63617c99488fa380fe1b1e4ded00bc1b4203
TorinSol <ethan.ricker567@gmail.com>
2023-11-08 00:26:29 -0500

    Added a function that checks for awoos in text, it is not done at all. Changed the Rfine function a little so that it recognizies the admin team as being real

--- src/main/FURRIT-BOT/main.py
+++ src/main/FURRIT-BOT/main.py
@@ -1,5 +1,7 @@
 import logging
 import random
+
+import telegram
 from telegram import Update, ChatMemberUpdated
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, ChatMemberHandler
 
@@ -35,6 +37,19 @@ async def pan(update: Update, context: ContextTypes.DEFAULT_TYPE):
             text="You need to reply to a message to pan."
         )
 
+async def autoAwoo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    #count = telegram.Bot.get_chat_member_count(update.effective_chat.id)
+    #count = telegram.Bot.getChatMemberCount(context.bot,update.effective_chat.id)
+    count = await context.bot.get_chat_member_count(update.effective_chat.id)
+    admins = await context.bot.get_chat_administrators(update.effective_chat.id)
+    text = "There are {} members in this chat.\n The admins of this chat are \n{}\n{}".format(count,admins[0].user.username,admins[1].user.username)
+    text1 = admins[0].user.username + admins[1].user.username
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=text
+    )
+    print(admins)
+
 #removes a single fine from a user
 # should allow the caller to specify the amt removed, also need to
 #   change so that you call it using the @ of a user.
@@ -43,12 +58,16 @@ async def Rfine( update: Update, context: ContextTypes.DEFAULT_TYPE):
     replied_message = update.message.reply_to_message
     user_says = "".join(context.args)
     #target = chat_member_update.difference().get("status")
+    admins = await context.bot.get_chat_administrators(update.effective_chat.id)
+    chat = await context.bot.get_chat(update.effective_chat.id)
+    print(chat.username)
+    name = '@' + admins[1].user.username
     if replied_message:
         original_message_id = replied_message.message_id
         await update.message.reply_text(
             text="forgiving a $350 fine from "+replied_message.from_user.username,
             reply_to_message_id=original_message_id)
-    elif user_says != "":
+    elif user_says == name:
         await context.bot.send_message(
 
             chat_id=update.effective_chat.id,
@@ -92,6 +111,7 @@ if __name__ == '__main__':
     application.add_handler(pan_handler)
     application.add_handler(fine_handler)
     application.add_handler(remove_fine_handler)
+    application.add_handler(CommandHandler("awoo",autoAwoo))
 
 
     application.run_polling()

c55abcee25e6bf06485b0305f605dfca4d7cd718
caden <cdk6546@rit.edu>
2023-11-11 17:07:04 -0500

    Added pan_count in database

--- src/main/FURRIT-BOT/db/users.sql
+++ src/main/FURRIT-BOT/db/users.sql
@@ -1,5 +1,6 @@
 CREATE TABLE USERS(
     telegram_id VARCHAR(64),
     username VARCHAR(64),
-    awoo_fine INTEGER DEFAULT 0
+    awoo_fine INTEGER DEFAULT 0,
+    pan_count INTEGER DEFAULT 0
     );

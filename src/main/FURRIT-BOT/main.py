11e37f47386e2532c0e87e5e0aa0f3be0c747ec1
caden <cdk6546@rit.edu>
2023-11-11 19:26:17 -0500

    Attempting to fix auto awoo

--- src/main/FURRIT-BOT/main.py
+++ src/main/FURRIT-BOT/main.py
@@ -50,7 +50,7 @@ async def pan(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 async def autoAwoo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text0 = update.message.text
-    t = re.findall(r"[a@]+[w]+[o0]+[o0]+",text0)
+    t = re.findall(r"[a]+[w]+[o0]+[o0]+",text0)
     if t:
         await context.bot.send_message(
             chat_id=update.effective_chat.id,

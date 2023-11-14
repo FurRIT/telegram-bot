df44e65f66f68d36898d43097ff7396f36b7e087
cdk6546 <cdk6546@rit.edu>
2023-11-13 19:48:33 -0500

    Added add_quote command for the bot

--- src/main/FURRIT-BOT/main.py
+++ src/main/FURRIT-BOT/main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import telegram
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, CallbackContext, MessageHandler, filters
-from db.users import add_current_members, get_members, rebuild_tables, add_pan_count
+from db.users import add_current_members, get_members, rebuild_tables, add_pan_count, add_quote_db
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -66,14 +66,53 @@ async def get_all_members(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+async def add_quote(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    replied_message = update.message.reply_to_message
+
+    try:
+        if replied_message:
+            if replied_message.from_user == update.message.from_user:
+                await context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text="You can't quote yourself."
+                )
+                return
+
+            if replied_message.from_user.id == context.bot.id:
+                await context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text="You can't quote the bot."
+                )
+                return
+
+            else:
+                quote_user_id = replied_message.from_user.id
+                quote_contents = replied_message.text
+                sender_user_id = update.message.from_user.id
+                add_quote_db(sender_user_id, quote_user_id, quote_contents)
+
+                await context.bot.send_message(
+                    chat_id=update.effective_chat.id,
+                    text="Quote added."
+                )
+    except Exception as e:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"Failed. {e}"
+        )
+
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token('6569990634:AAEJ2MLYy-ByCOjHbqzzfFyIbUvqi5zDUcU').build()
     pan_handler = CommandHandler('pan', pan)
     get_handler = CommandHandler('get', get_all_members)
+    add_quote_handler = CommandHandler('addquote', add_quote)
     members_handler = MessageHandler(filters.CHAT, handle_messages)
+    rebuild_tables()
 
     application.add_handler(pan_handler)
     application.add_handler(get_handler)
     application.add_handler(members_handler)
+    application.add_handler(add_quote_handler)
 
     application.run_polling()

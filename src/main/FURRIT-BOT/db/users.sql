915daee87c03d02d68555d302fb764372cf8e2a3
cdk6546 <cdk6546@rit.edu>
2023-11-13 19:49:06 -0500

    Modified the schema to contain a quotes table

--- src/main/FURRIT-BOT/db/users.sql
+++ src/main/FURRIT-BOT/db/users.sql
@@ -1,8 +1,16 @@
 DROP TABLE IF EXISTS USERS;
+DROP TABLE IF EXISTS QUOTES;
 
 CREATE TABLE USERS(
-    telegram_id VARCHAR(64),
+    telegram_id VARCHAR(64) PRIMARY KEY,
     username VARCHAR(64),
     awoo_fine INTEGER DEFAULT 0,
     pan_count INTEGER DEFAULT 0
+    );
+
+CREATE TABLE QUOTES(
+    telegram_id VARCHAR(64) REFERENCES USERS(telegram_id),
+    quote VARCHAR(265),
+    date_issued DATE,
+    issued_by_id VARCHAR(64) REFERENCES USERS(telegram_id)
     );

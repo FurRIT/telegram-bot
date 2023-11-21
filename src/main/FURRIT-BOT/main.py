f607f3911f37ab124773a433b2d6fbddfe7835e0
TorinSol <ethan.ricker567@gmail.com>
2023-11-21 13:33:27 -0500

    added the value to the fine commands

--- src/main/FURRIT-BOT/main.py
+++ src/main/FURRIT-BOT/main.py
@@ -61,7 +61,7 @@ async def auto_awoo(update: Update, context: ContextTypes.DEFAULT_TYPE):
             print(x[0])
             if int(update.message.from_user.id) == int(x[0]):
                 print(type(x[2]))
-                #x[2] += 350
+                x[2] += 350
                 await context.bot.send_message(
                     chat_id=update.effective_chat.id,
                     text="Don't Awoo! - $350 fine!\n\n{}'s current fines {}".format(update.message.from_user.first_name,

2700f5818b41070a69d70d554cf0cbc59800e496
TorinSol <ethan.ricker567@gmail.com>
2024-01-29 18:11:51 -0500

    Trying to fix the fine bug

--- src/main/FURRIT-BOT/main.py
+++ src/main/FURRIT-BOT/main.py
@@ -138,12 +138,12 @@ async def fines(update: Update, context: ContextTypes.DEFAULT_TYPE):
             if int(update.message.reply_to_message.message_id) == int(x[0]):
                 add_fine(x[0])
 
-        await context.bot.send_message(#test function, remove or make unused
-            chat_id=update.effective_chat.id,
-            text="TEST\nx[0] = {}\nx[1]={}\nx[2] = {} (fine value)".format(x[0],x[1],x[2]))
+            await context.bot.send_message(#test function, remove or make unused
+                chat_id=update.effective_chat.id,
+                text="TEST\nx[0] = {}\nx[1]={}\nx[2] = {} (fine value)".format(x[0],x[1],x[2]))
 
-        await update.message.reply_text(text="Fining " + user + " $350\n\n{}'s current fines ${}".format(replied_message.from_user.first_name,
-                                                                           x[2] + 350), reply_to_message_id=original_message_id)
+            await update.message.reply_text(text="Fining " + user + " $350\n\n{}'s current fines ${}".format(replied_message.from_user.first_name,
+                                                                               x[2] + 350), reply_to_message_id=original_message_id)
 
     else:
         await context.bot.send_message(

b15b935c0276873518544a01663fcc512bbabe77
TorinSol <ethan.ricker567@gmail.com>
2024-02-11 18:44:51 -0500

    changed the user sql to allow for adding all the other users. Also, rebuilt the table

--- src/main/FURRIT-BOT/db/users.sql
+++ src/main/FURRIT-BOT/db/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE USERS(
     telegram_id VARCHAR(64),
     firstName VARCHAR(64),
-    lastName VARCHAR(64)
-    username VARCHAR(64)
+    lastName VARCHAR(64),
+    username VARCHAR(64),
     awoo_fine INTEGER DEFAULT 0
     );

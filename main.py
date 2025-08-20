06e81949a86add05ab75dd97bcfe91e9f326b6a4
tristen <123990328+tristenkurutz@users.noreply.github.com>
2025-08-20 16:50:55 -0400

    Merging [env variables branch](https://github.com/FurRIT/telegram-bot/pull/13) in here since it's getting merged soon
    Signed-off-by: tristen <123990328+tristenkurutz@users.noreply.github.com>

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from datetime import datetime, timedelta  # imported for /ban method
 
 import dotenv
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
 from telegram import Update
 
 from telegram.ext import (

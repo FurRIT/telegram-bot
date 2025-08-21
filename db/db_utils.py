73c9b91f25b19f8288f1b6602ee8bce1b87e4ea6
minkedalt <gh.alt@minkedup.xyz>
2025-08-21 16:53:33 -0400

    Removed completely unused functions in db_utils.py

--- db/db_utils.py
+++ db/db_utils.py
@@ -29,32 +29,3 @@ def exec_sql_file(path: str) -> None:
 
     conn.commit()
     conn.close()
-
-
-def exec_get_one(sql, args={}):
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute(sql, args)
-    one = cur.fetchone()
-    conn.close()
-    return one
-
-
-def exec_get_all(sql, args={}):
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute(sql, args)
-    # https://www.psycopg.org/docs/cursor.html#cursor.fetchall
-
-    list_of_tuples = cur.fetchall()
-    conn.close()
-    return list_of_tuples
-
-
-def exec_commit(sql, args={}):
-    conn = connect()
-    cur = conn.cursor()
-    result = cur.execute(sql, args)
-    conn.commit()
-    conn.close()
-    return result

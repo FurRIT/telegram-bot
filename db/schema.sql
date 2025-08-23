9861b1c6a42fa15f046fbe5b428bccd9bb66fd92
minkedalt <gh.alt@minkedup.xyz>
2025-08-23 18:20:16 -0400

    Adjusted datatypes to account for telegram limits

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,22 @@
 DROP TABLE IF EXISTS QUOTES;
 DROP TABLE IF EXISTS USERS;
 
+/* NOTE: limits taken from https://limits.tginfo.me/en */
 CREATE TABLE USERS(
-    telegram_id     VARCHAR(64),
-    firstName       VARCHAR(64),
-    lastName        VARCHAR(64),
-    username        VARCHAR(64),
-    awoo_fine       INTEGER DEFAULT 0,
-    pan_count       INTEGER DEFAULT 0
+    id              INTEGER PRIMARY KEY,
+    tg_id           INTEGER NOT NULL UNIQUE,
+    tg_first_name   VARCHAR(64) NOT NULL,
+    tg_last_name    VARCHAR(64) NOT NULL,
+    tg_username     VARCHAR(32) NOT NULL,
+    awoo_debt       INTEGER DEFAULT 0 NOT NULL,
+    pan_count       INTEGER DEFAULT 0 NOT NULL
 );
 
 CREATE TABLE QUOTES(
-    telegram_id     VARCHAR(64) REFERENCES USERS(telegram_id),
-    quote           VARCHAR(265),
-    date_issued     VARCHAR(64),
-    issued_by_id    VARCHAR(64) REFERENCES USERS(telegram_id)
+    id              INTEGER PRIMARY KEY,
+    sent_by         INTEGER REFERENCES USERS(id) NOT NULL,
+    sent_at         INTEGER NOT NULL,
+    added_by        INTEGER REFERENCES USERS(id) NOT NULL,
+    added_at        INTEGER NOT NULL,
+    quote           VARCHAR(256) NOT NULL,
 );

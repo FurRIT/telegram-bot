d7ddc2dd237f6cf938ebf447e69a626b6035fd4c
minkedalt <gh.alt@minkedup.xyz>
2025-08-23 18:50:18 -0400

    Significantly refactored users.py for new changes

--- db/users.py
+++ db/users.py
@@ -21,157 +21,163 @@ def rebuild_tables() -> None:
     exec_sql_file(SCHEMA_PATH)
 
 
-def add_pan_count(uid: int) -> None:
+def add_pan_count(tg_id: int) -> None:
     """
     Increment the pan count for a User.
     """
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"""SELECT PAN_COUNT FROM USERS WHERE TELEGRAM_ID = "{uid}" """)
-    count = cursor.fetchone()[0]
-    new_count = count + 1
+    con = connect()
+    cur = con.cursor()
 
-    cursor.execute(
-        f"""UPDATE USERS SET PAN_COUNT = {new_count} 
-                   WHERE TELEGRAM_ID = "{uid}" """
-    )
-    conn.commit()
-    conn.close()
+    cur.execute("SELECT pan_count FROM USERS WHERE tg_id = ?", (tg_id,))
+    res = cur.fetchone()
 
+    if res is None:
+        cur.close()
+        con.close()
+        return
 
-def add_current_members(username: str, uid: int, fname: str, lname: str) -> int:
-    """
-    Ensure that a User exists in the Database.
-    """
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"""SELECT USERNAME FROM USERS WHERE TELEGRAM_ID = {str(uid)}""")
-    existence = cursor.fetchone()
+    pan_count = res[0]
+    pan_count += 1
 
-    if not existence:
-        cursor.execute(
-            f"""INSERT INTO USERS VALUES ("{uid}","{fname}","{lname}","{username}",0,0)"""
-        )
-        conn.commit()
-        conn.close()
-        return 1
+    cur.execute("UPDATE USERS SET pan_count = ? WHERE tg_id = ?", (pan_count, tg_id))
 
-    else:
-        conn.commit()
-        conn.close()
-        return 0
+    con.commit()
+    con.close()
 
 
-def add_quote_db(from_uid: int, to_uid: int, quote: str) -> int:
+def add_quote_db(from_uid: int, to_uid: int, quote: str) -> bool:
     """
     Add a Quote to the Database.
     """
 
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"""SELECT TELEGRAM_ID FROM QUOTES WHERE QUOTE = "{quote}" """)
-    existence = cursor.fetchone()
+    con = connect()
+    cur = con.cursor()
 
-    if not existence:
-        cursor.execute(
-            f"""INSERT INTO QUOTES VALUES ({to_uid}, "{quote}",{datetime.date.today()}, {from_uid}) """
-        )
-        conn.commit()
-        conn.close()
-        return 1
-    else:
-        conn.close()
-        return 0
+    cur.execute(f"""SELECT user_id FROM QUOTES WHERE QUOTE = "{quote}" """)
+    res = cur.fetchone()
+
+    if res is None:
+        cur.close()
+        con.close()
+        return False
+
+    cur.execute(
+        f"""INSERT INTO QUOTES VALUES ({to_uid}, "{quote}",{datetime.date.today()}, {from_uid}) """
+    )
+
+    con.commit()
+    cur.close()
+    con.close()
+
+    return True
 
 
 def get_members():
     """
     Get all Users.
     """
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM USERS")
-    existence = cursor.fetchall()
-    return existence
+    con = connect()
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM USERS")
+    res = cur.fetchall()
+
+    cur.close()
+    con.close()
+
+    return res
 
 
-def add_fines(uid: int, fine: int) -> int:
+def add_fines(tg_id: int, fine: int) -> bool:
     """
     Add a fine amount to a User.
     """
 
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"""SELECT AWOO_FINE FROM USERS WHERE TELEGRAM_ID = {str(uid)} """)
-    logging.info(fine)
-    existence = cursor.fetchone()
-    if existence:
-        cursor.execute(
-            f"UPDATE USERS SET AWOO_FINE = AWOO_FINE + %s WHERE TELEGRAM_ID = %s",
-            (
-                int(fine),
-                str(uid),
-            ),
-        )
-        conn.commit()
-        conn.close()
-        return 1
-    else:
-        conn.commit()
-        conn.close()
-        return 0
+    con = connect()
+    cur = con.cursor()
+
+    cur.execute("SELECT awoo_debt FROM USERS WHERE tg_id = ?", (tg_id,))
+    res = cur.fetchone()
+    if res is None:
+        cur.close()
+        con.close()
+        return False
+
+    cur.execute(
+        "UPDATE USERS SET awoo_debt = awoo_debt + ? WHERE tg_id = ?",
+        (
+            fine,
+            tg_id,
+        ),
+    )
+
+    con.commit()
+    cur.close()
+    con.close()
+
+    return True
 
 
-def add_fine(uid: int) -> int:
+def add_fine(tg_id: int) -> bool:
     """
     Add a fine of 350 to a User.
     """
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"""SELECT AWOO_FINE FROM USERS WHERE TELEGRAM_ID = "{str(uid)}" """)
-    existence = cursor.fetchone()
-    if existence:
-        cursor.execute(
-            f"""UPDATE USERS SET AWOO_FINE = AWOO_FINE + {350} WHERE TELEGRAM_ID = "{str(uid)}" """
-        )
-        conn.commit()
-        conn.close()
-        return 1
-    else:
-        conn.commit()
-        conn.close()
-        return 0
+    con = connect()
+    cur = con.cursor()
+    cur.execute("SELECT awoo_debt FROM USERS WHERE tg_id = ?", (tg_id,))
+    res = cur.fetchone()
+    if res is None:
+        cur.close()
+        con.close()
+        return False
+
+    cur.execute(
+        f"""UPDATE USERS SET AWOO_FINE = AWOO_FINE + {350} WHERE TELEGRAM_ID = "{str(tg_id)}" """
+    )
+
+    con.commit()
+    cur.close()
+    con.close()
+
+    return True
 
 
-def remove_fine(amt: int, uid: int):
+def remove_fine(amt: int, tg_id: int) -> bool:
     """
     Forgive a fine of some amount.
     """
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute(f"SELECT AWOO_FINE FROM USERS WHERE TELEGRAM_ID = {str(uid)}")
-    existence = cursor.fetchone()
-    if existence:
-        cursor.execute(
-            "UPDATE USERS SET AWOO_FINE = AWOO_FINE - {} WHERE TELEGRAM_ID = '{}'".format(
-                amt, str(uid)
-            )
-        )
-        conn.commit()
-        conn.close()
-        return 1
-    else:
-        conn.commit()
-        conn.close()
-        return 0
+    con = connect()
+    cur = con.cursor()
+
+    cur.execute("SELECT awoo_debt FROM USERS WHERE tg_id = ?", (tg_id,))
+    res = cur.fetchone()
+    if res is None:
+        cur.close()
+        con.close()
+        return False
+
+    cur.execute(
+        "UPDATE USERS SET AWOO_FINE = AWOO_FINE - ? WHERE tg_id = ?", (amt, tg_id)
+    )
+
+    con.commit()
+    cur.close()
+    con.close()
+
+    return True
 
 
 def get_quotes():
     """
     Get all Quotes.
     """
-    conn = connect()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM QUOTES")
-    existence = cursor.fetchall()
-    return existence
+    con = connect()
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM QUOTES")
+    res = cur.fetchall()
+
+    cur.close()
+    con.close()
+
+    return res

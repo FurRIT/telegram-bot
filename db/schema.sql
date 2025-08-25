b925bfe0de9589ccf478805025d5067ff53a8fa2
minkedalt <gh.alt@minkedup.xyz>
2025-08-24 20:56:33 -0400

    Increased max quote len to 512 characters

--- db/schema.sql
+++ db/schema.sql
@@ -21,5 +21,5 @@ CREATE TABLE QUOTES(
     quoter_tg_id        INTEGER REFERENCES USERS(tg_id) NOT NULL,
     quoter_msg_sent_at  TEXT NOT NULL,
     quoter_msg_id       INTEGER NOT NULL,
-    quote               VARCHAR(256) NOT NULL
+    quote               VARCHAR(512) NOT NULL
 );

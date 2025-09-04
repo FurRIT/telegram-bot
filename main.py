540208766d8b7808820230eed74d5fbdf6da2083
Sol <ethan.ricker567@gmail.com>
2025-09-04 14:54:14 -0400

    Added a files that stores a list of admin ids and added a conditional to the fine command to stop unauthorized use

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ from messages import (
     COMMANDS_MESSAGE,
 )
 
+from admins import (ADMINS_IDS)
+
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
@@ -496,6 +498,15 @@ async def cmd_fine(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     effective_chat = update.effective_chat
     assert effective_chat is not None
+
+    user_id = message.from_user.id
+    assert user_id is not None
+    if user_id not in ADMINS_IDS:
+        await context.bot.send_message(
+            chat_id=effective_chat.id, text="Only admins can add fines"
+        )
+        return
+
 
     reply_to_message = message.reply_to_message
     if reply_to_message is None:

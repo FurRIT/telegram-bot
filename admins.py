ee6f749a832c0f68ca2087e2ea34de5504fae92d
minkedalt <alt.gh@minkedup.xyz>
2025-09-16 22:12:19 -0400

    Reformatted admins file

--- admins.py
+++ admins.py
@@ -4,15 +4,9 @@ Stores a list of current admins along with there telegram IDs
 Used for commands and such that only an admin should be able to run
 """
 
-#Torin 5342561124
-#Mitz 1731152417
-#Sparks 5342054924
-#Baliey 5485529202
-#Charles 2076251814
-ADMINS_IDS = {
-        5342561124,
-        1731152417,
-        5342054924,
-        5485529202,
-        2076251814
-        }
+# Torin 5342561124
+# Mitz 1731152417
+# Sparks 5342054924
+# Baliey 5485529202
+# Charles 2076251814
+ADMINS_IDS = {5342561124, 1731152417, 5342054924, 5485529202, 2076251814}

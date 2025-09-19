dffa8b4a3b79511c91b8b389ad6b654ef9f37f40
minkedalt <alt.gh@minkedup.xyz>
2025-09-19 11:05:30 -0400

    Added msgs_dir as a configuration attribute

--- src/furrit/config.py
+++ src/furrit/config.py
@@ -17,6 +17,7 @@ class Config:
     admin_cid: int
     bot_token: str
     admin_ids: frozenset[int]
+    msgs_dir: str
 
 
 def load_config(path: str) -> tuple[Config, None] | tuple[None, str]:
@@ -51,12 +52,16 @@ def load_config(path: str) -> tuple[Config, None] | tuple[None, str]:
         if not isinstance(item, int):
             return (None, f".admin_ids[{i}] must be of type int")
 
+    if not ("msgs_dir" in raw and isinstance(raw["msgs_dir"], str)):
+        return (None, ".msgs_dir must exist and be of type str")
+
     r_admin_ids: list[int] = raw["admin_ids"]
     admin_ids = frozenset(r_admin_ids)
 
     cid: int = raw["cid"]
     admin_cid: int = raw["admin_cid"]
     bot_token: str = raw["bot_token"]
+    msgs_dir: str = raw["msgs_dir"]
 
-    config = Config(cid, admin_cid, bot_token, admin_ids)
+    config = Config(cid, admin_cid, bot_token, admin_ids, msgs_dir)
     return (config, None)

235d9f5ff8dec7d4a1771d6b4cb6b2b83e9f2e65
minkedalt <gh.alt@minkedup.xyz>
2025-09-19 22:16:36 -0400

    Added message module for loading messages

---
+++ src/furrit/message.py
@@ -0,0 +1,51 @@
+"""
+Static Message Utilities.
+"""
+
+import os
+import os.path
+import dataclasses
+
+import telegram.helpers
+
+
+@dataclasses.dataclass(frozen=True)
+class MessageStore:
+    """
+    In-Memory Store of Messages.
+    """
+
+    mapping: dict[str, str]
+
+    def get(self, prefix: str) -> str:
+        """
+        Get a message corresponding to a message.
+        """
+        if prefix not in self.mapping:
+            raise ValueError(f"message {prefix} does not exist")
+
+        return self.mapping[prefix]
+
+
+def load_store_dir(path: str) -> MessageStore:
+    """
+    Load a MessageStore from a directory.
+    """
+
+    messages = {}
+
+    for node in os.listdir(path):
+        node_path = os.path.join(path, node)
+        if not os.path.isfile(node_path):
+            continue
+
+        prefix, postfix = os.path.splitext(node)
+        if postfix != ".html":
+            continue
+
+        with open(node_path, "r", encoding="utf-8") as file:
+            txt = file.read()
+
+        messages[prefix] = txt
+
+    return MessageStore(messages)

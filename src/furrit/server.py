620c7aa2458f9682a2b0c809fc48741ea1ff7207
minkedalt <alt.gh@minkedup.xyz>
2025-09-27 17:59:45 -0400

    Added editing of already-existing event msgs

--- src/furrit/server.py
+++ src/furrit/server.py
@@ -3,6 +3,7 @@ HTTP Server.
 """
 
 from typing import cast
+import logging
 
 import aiohttp
 import aiohttp.web
@@ -39,33 +40,35 @@ async def post_event(request: aiohttp.web.Request):
     m_event_row = try_get_event_by_ext_id(raw_event["uid"])
     msg_txt = raw_event_to_msg_text(raw_event)
 
+    is_new_event: bool = m_event_row is None
     if m_event_row is None:
-        event_row = insert_event(raw_event["uid"])
+        m_event_row = insert_event(raw_event["uid"])
 
-        keyboard = [
-            [
-                telegram.InlineKeyboardButton(
-                    "Yes",
-                    callback_data=EventCallbackData(
-                        event_row.id, EventReactionKind.YES
-                    ).to_json(),
-                ),
-                telegram.InlineKeyboardButton(
-                    "Maybe",
-                    callback_data=EventCallbackData(
-                        event_row.id, EventReactionKind.MAYBE
-                    ).to_json(),
-                ),
-                telegram.InlineKeyboardButton(
-                    "No",
-                    callback_data=EventCallbackData(
-                        event_row.id, EventReactionKind.NO
-                    ).to_json(),
-                ),
-            ]
+    keyboard = [
+        [
+            telegram.InlineKeyboardButton(
+                "Yes",
+                callback_data=EventCallbackData(
+                    m_event_row.id, EventReactionKind.YES
+                ).to_json(),
+            ),
+            telegram.InlineKeyboardButton(
+                "Maybe",
+                callback_data=EventCallbackData(
+                    m_event_row.id, EventReactionKind.MAYBE
+                ).to_json(),
+            ),
+            telegram.InlineKeyboardButton(
+                "No",
+                callback_data=EventCallbackData(
+                    m_event_row.id, EventReactionKind.NO
+                ).to_json(),
+            ),
         ]
-        reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+    ]
+    reply_markup = telegram.InlineKeyboardMarkup(keyboard)
 
+    if is_new_event:
         main_msg = await ctx.application.bot.send_message(
             chat_id=ctx.main_cid,
             text=msg_txt,
@@ -85,14 +88,19 @@ async def post_event(request: aiohttp.web.Request):
             reply_markup=reply_markup,
         )
 
-        insert_event_message(event_row.id, main_msg.id, ctx.main_cid)
-        insert_event_message(event_row.id, events_msg.id, ctx.events_cid)
+        insert_event_message(m_event_row.id, main_msg.id, ctx.main_cid)
+        insert_event_message(m_event_row.id, events_msg.id, ctx.events_cid)
 
         return aiohttp.web.Response(status=200)
 
     event_messages = try_get_event_messages_by_event_id(m_event_row.id)
     for event_message in event_messages:
-        # TODO: edit existing messages with new information
-        pass
+        await ctx.application.bot.edit_message_text(
+            msg_txt,
+            chat_id=event_message.tg_chat_id,
+            message_id=event_message.tg_msg_id,
+            parse_mode="HTML",
+            reply_markup=reply_markup,
+        )
 
     return aiohttp.web.Response(status=201)
